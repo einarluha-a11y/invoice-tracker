415c5d6b1e58f3c9fda2d1f2b1764236b2d9f318
Einar Luha <einarluha@Mac.lan>
2026-03-21 22:34:36 +0200

    chore: major audit and cleanup of development scratchpads

--- automation/test_vies.cjs
+++
@@ -1,9 +0,0 @@
-const { validateVat } = require('./vies_validator.cjs');
-
-async function testVat(vat) {
-    console.log(`Testing VAT: ${vat}`);
-    const res = await validateVat(vat);
-    console.log(JSON.stringify(res, null, 2));
-}
-
-testVat('IE6388047V'); // Google Ireland
